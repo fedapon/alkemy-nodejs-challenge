f4002c04d762f4d320695464a298b06b8fd350d7
fedapon <fedapon@gmail.com>
2022-01-11 16:01:16 -0300

    added sequilize configuration

---
+++ src/config/database.js
@@ -0,0 +1,10 @@
+import { Sequelize } from 'sequelize'
+
+
+const db = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'database.sqlite',
+    logging: false,                  // Default, displays the first parameter of the log function call
+})
+
+export default db
--- src/index.js
+++ src/index.js
@@ -1,29 +1,27 @@
 import express from 'express'
 import cors from 'cors'
-import { Sequelize } from 'sequelize'
+import db from './config/database.js'
+import Character from './models/character.model.js'
+import Movie from './models/movie.model.js'
 import charactersRoute from './routes/characters.route.js'
 import moviesRoute from './routes/movies.route.js'
 
 const app = express()
-export const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: '../persistence/database.sqlite',
-    logging: console.log,                  // Default, displays the first parameter of the log function call
-})
+
+Character.belongsToMany(Movie, {through: 'CharactersMovies'})
+Movie.belongsToMany(Character, {through: 'CharactersMovies'})
+
+db.authenticate()
+    .then(async ()=>{
+        await db.sync({ alter: true })
+        console.log('Database connected...')
+    })
+    .catch(error=>console.log('Database connection error: ' + error))
 
 //middlewares
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
-app.get('/', async (req, res) => {
-    try {
-        await sequelize.authenticate()
-        res.send('Connection has been established successfully.')
-    } catch (error) {
-        res.send('Unable to connect to the database: ' + error)
-    }
-})
 
 app.use('/characters', charactersRoute)
 
