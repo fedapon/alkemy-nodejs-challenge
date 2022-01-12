419a4ca27bb66ce4bccb64b17540afb0df32e75c
fedapon <fedapon@gmail.com>
2022-01-12 19:05:56 -0300

    dotenv and jwt installed

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
+import dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
 import db from './config/database.js'
 import databaseConfiguration from './config/database.config.js'
+import authRoute from './routes/auth.route.js'
 import charactersRoute from './routes/characters.route.js'
 import moviesRoute from './routes/movies.route.js'
 
+dotenv.config()
 const app = express()
+const PORT = process.env.PORT | 3000
 
-databaseConfiguration(db, {mockdata: true})
+databaseConfiguration(db, { mockdata: true })
 
 //middlewares
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+app.use('/auth', authRoute)
+
 app.use('/characters', charactersRoute)
 
 app.use('/movies', moviesRoute)
 
-app.listen(3000, function () {
-    console.log('Server started on port 3000')
+app.listen(PORT, function () {
+    console.log(`Server started on port ${PORT}`)
 })
