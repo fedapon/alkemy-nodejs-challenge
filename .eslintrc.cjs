c2152ee903fa86e144ae3168dbdfdef85b0811d8
fedapon <fedapon@gmail.com>
2022-01-12 19:50:40 -0300

    eslint & prettier plugin installed

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+    env: {
+        es2021: true,
+        node: true
+    },
+    extends: ['plugin:prettier/recommended'],
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module'
+    },
+    plugins: ['prettier'],
+    rules: {
+        'prettier/prettier': [
+            'error',
+            {
+                trailingComma: 'none',
+                tabWidth: 4,
+                useTabs: false,
+                semi: false,
+                singleQuote: true,
+                arrowParens: 'always'
+            }
+        ]
+    }
+}
