b6f9ac9fa8874d8fe22a7b18c4af4ffda2fac5c0
fedapon <fedapon@gmail.com>
2022-01-13 16:13:36 -0300

    mockdata command now is in .env file

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT | 3000
 
-databaseConfiguration(db, { mockdata: true })
+databaseConfiguration(db, { mockdata: process.env.MOCKDATA })
 
 //middlewares
 app.use(cors())
