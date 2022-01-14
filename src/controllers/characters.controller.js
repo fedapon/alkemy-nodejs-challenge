5c8756a841a7a3b471be51251a73294d8cc9f60c
fedapon <fedapon@gmail.com>
2022-01-14 15:39:38 -0300

    modified status responses

--- src/controllers/characters.controller.js
+++ src/controllers/characters.controller.js
@@ -3,37 +3,51 @@ import charactersService from '../services/characters.service.js'
 const db = new charactersService()
 
 export async function getAllCharacters(req, res) {
-    return res.json(await db.getAllCharacters(req.query))
+    return res.status(200).json(await db.getAllCharacters(req.query))
 }
 
 export async function getCharacter(req, res) {
-    return res.json(await db.getCharacterById(req.params.id))
+    return res.status(200).json(await db.getCharacterById(req.params.id))
 }
 
 export async function createCharacter(req, res) {
     const { picture, name, age, weigth, history } = req.body
-    return res.json(
-        await db.createCharacter({ picture, name, age, weigth, history })
-    )
-}
-
-export async function editCharacter(req, res) {
-    const { picture, name, age, weigth, history } = req.body
-    return res.json(
-        await db.editCharacterById(req.params.id, {
+    try {
+        const newCharacter = await db.createCharacter({
             picture,
             name,
             age,
             weigth,
             history
         })
-    )
+        return res.status(201).json(newCharacter)
+    } catch (error) {
+        return res.status(400).json({ error: error.errors[0].message })
+    }
+}
+
+export async function editCharacter(req, res) {
+    const { picture, name, age, weigth, history } = req.body
+    try {
+        const editedCharacter = await db.editCharacterById(req.params.id, {
+            picture,
+            name,
+            age,
+            weigth,
+            history
+        })
+        return res.status(200).json(editedCharacter)
+    } catch (error) {
+        return res.status(400).json({ error: error.errors[0].message })
+    }
 }
 
 export async function deleteCharacter(req, res) {
     if (await db.deleteCharacterById(req.params.id)) {
-        return res.json({ message: 'character deleted' })
+        return res.status(200).json({ message: 'character deleted' })
     } else {
-        return res.json({ message: 'character could not be deleted' })
+        return res
+            .status(400)
+            .json({ message: 'character could not be deleted' })
     }
 }
