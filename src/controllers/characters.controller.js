02dc117359a27c6710c284f488cd2c58281da656
fedapon <fedapon@gmail.com>
2022-01-14 15:01:58 -0300

    destruturing of body params to create and edit characters

--- src/controllers/characters.controller.js
+++ src/controllers/characters.controller.js
@@ -3,21 +3,37 @@ import charactersService from '../services/characters.service.js'
 const db = new charactersService()
 
 export async function getAllCharacters(req, res) {
-    res.json(await db.getAllCharacters(req.query))
+    return res.json(await db.getAllCharacters(req.query))
 }
 
 export async function getCharacter(req, res) {
-    res.json(await db.getCharacterById(req.params.id))
+    return res.json(await db.getCharacterById(req.params.id))
 }
 
 export async function createCharacter(req, res) {
-    res.json(await db.createCharacter(req.body))
+    const { picture, name, age, weigth, history } = req.body
+    return res.json(
+        await db.createCharacter({ picture, name, age, weigth, history })
+    )
 }
 
 export async function editCharacter(req, res) {
-    res.json(await db.editCharacterById(req.params.id, req.body))
+    const { picture, name, age, weigth, history } = req.body
+    return res.json(
+        await db.editCharacterById(req.params.id, {
+            picture,
+            name,
+            age,
+            weigth,
+            history
+        })
+    )
 }
 
 export async function deleteCharacter(req, res) {
-    res.json(await db.deleteCharacterById(req.params.id))
+    if (await db.deleteCharacterById(req.params.id)) {
+        return res.json({ message: 'character deleted' })
+    } else {
+        return res.json({ message: 'character could not be deleted' })
+    }
 }
